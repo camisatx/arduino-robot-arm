5b0c86fefe0048c00efe941bf72f84ddeafd2712
camisatx <joshschertz3@gmail.com>
2020-01-23 11:41:23 -0500

    Improve controller lcd messages

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -32,6 +32,48 @@ uint8_t data[6];
 //Define the message buffer
 uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
 
+//LCD characters
+byte CharUp[8] = {
+  0b00000,
+  0b00100,
+  0b01110,
+  0b11111,
+  0b00100,
+  0b00100,
+  0b00100,
+  0b00100
+};
+byte CharDown[8] = {
+  0b00100,
+  0b00100,
+  0b00100,
+  0b00100,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00000
+};
+byte CharLeft[8] = {
+  0b00000,
+  0b00100,
+  0b01100,
+  0b11111,
+  0b01100,
+  0b00100,
+  0b00000,
+  0b00000
+};
+byte CharRight[8] = {
+  0b00000,
+  0b00100,
+  0b00110,
+  0b11111,
+  0b00110,
+  0b00100,
+  0b00000,
+  0b00000
+};
+
 void setup() {
   Serial.begin(9600);
 
@@ -42,6 +84,11 @@ void setup() {
   lcd.backlight();
   lcd.clear();
   lcd.print("Initializing...");
+  //create lcd characters
+  lcd.createChar(0, CharUp);
+  lcd.createChar(1, CharDown);
+  lcd.createChar(2, CharLeft);
+  lcd.createChar(3, CharRight);
 
   //initialize manager object
   if (!manager.init())
@@ -56,22 +103,76 @@ void setup() {
 
   //initialize the joystick buttons
   pinMode(LEFT_JOY_SW, INPUT);
+  digitalWrite(LEFT_JOY_SW, HIGH);
   pinMode(RIGHT_JOY_SW, INPUT);
+  digitalWrite(RIGHT_JOY_SW, HIGH);
 
   Serial.println("Initialized");
   lcd.clear();
-  lcd.print("Initialized...");
+  lcd.print("L:");
+  lcd.setCursor(8, 0);
+  lcd.print("R:");
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-
   //Read the analog input, map range from 0-1023 to 0-255
-  data[0] = map(analogRead(LEFT_JOY_X), 0, 1023, 0, 255);
-  data[1] = map(analogRead(LEFT_JOY_Y), 0, 1023, 0, 255);
+  int left_joy_x_reading = analogRead(LEFT_JOY_X);
+  if (left_joy_x_reading < 470 || left_joy_x_reading > 550) {
+    data[0] = map(left_joy_x_reading, 0, 1023, 0, 255);
+    lcd.setCursor(3, 0);
+    if (data[0] < 127) {
+      lcd.write(byte(2));
+    } else {
+      lcd.write(byte(3));
+    }
+  } else {
+    data[0] = 127;
+    lcd.setCursor(3, 0);
+    lcd.print(" ");
+  }
+  int left_joy_y_reading = analogRead(LEFT_JOY_Y);
+  if (left_joy_y_reading < 470 || left_joy_y_reading > 550) {
+    data[1] = map(left_joy_y_reading, 0, 1023, 0, 255);
+    lcd.setCursor(5, 0);
+    if (data[1] < 127) {
+      lcd.write(byte(1));
+    } else {
+      lcd.write(byte(0));
+    }
+  } else {
+    data[1] = 127;
+    lcd.setCursor(5, 0);
+    lcd.print(" ");
+  }
+  int right_joy_x_reading = analogRead(RIGHT_JOY_X);
+  if (right_joy_x_reading < 470 || right_joy_x_reading > 550) {
+    data[3] = map(right_joy_x_reading, 0, 1023, 0, 255);
+    lcd.setCursor(11, 0);
+    if (data[3] < 127) {
+      lcd.write(byte(2));
+    } else {
+      lcd.write(byte(3));
+    }
+  } else {
+    data[3] = 127;
+    lcd.setCursor(11, 0);
+    lcd.print(" ");
+  }
+  int right_joy_y_reading = analogRead(RIGHT_JOY_Y);
+  if (right_joy_y_reading < 470 || right_joy_y_reading > 550) {
+    data[4] = map(right_joy_y_reading, 0, 1023, 0, 255);
+    lcd.setCursor(13, 0);
+    if (data[4] < 127) {
+      lcd.write(byte(1));
+    } else {
+      lcd.write(byte(0));
+    }
+  } else {
+    data[4] = 127;
+    lcd.setCursor(13, 0);
+    lcd.print(" ");
+  }
   data[2] = digitalRead(LEFT_JOY_SW);
-  data[3] = map(analogRead(RIGHT_JOY_X), 0, 1023, 0, 255);
-  data[4] = map(analogRead(RIGHT_JOY_Y), 0, 1023, 0, 255);
   data[5] = digitalRead(RIGHT_JOY_SW);
 
   //Display the joystick data
@@ -88,19 +189,9 @@ void loop() {
   Serial.print(data[4]);
   Serial.print("\tRSW: ");
   Serial.println(data[5]);
-
   Serial.println("Sending joystick data");
-  lcd.setCursor(0, 0);
-  lcd.print("                ");  //clear row
-  lcd.setCursor(0, 0);
-  lcd.print(data[0]);
-  lcd.print(";");
-  lcd.print(data[1]);
-  lcd.print(";");
-  lcd.print(data[3]);
-  lcd.print(";");
-  lcd.print(data[4]);
 
+  digitalWrite(13, HIGH);
   if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS)) {
     uint8_t len = sizeof(buf);
     uint8_t from;
@@ -110,17 +201,20 @@ void loop() {
       Serial.print(": ");
       Serial.println((char*)buf);
       lcd.setCursor(0, 1);
-      lcd.print("R:");
-      lcd.print((char*)buf);
+      lcd.print("LK: ");  //link to base status
+      lcd.print(buf[0]);
+      lcd.setCursor(8, 1);
+      lcd.print("ST: ");  //base state
+      lcd.print(buf[1]);
     } else {
       Serial.println("No reply. Is base nrf24 running?");
     }
   } else {
     Serial.println("Recv failed");
     lcd.setCursor(0, 1);
-    lcd.print("Recv failed");
+    lcd.print("LK: NA  ST: NA");
   }
 
   digitalWrite(13, LOW);
-  delay(50);
+  delay(100);
 }
