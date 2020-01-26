4062e2b4a99a1964c0fa5eb50b1d1678ba7d8986
camisatx <joshschertz3@gmail.com>
2020-01-25 22:10:35 -0500

    Add all servos to base; change from servo angle to microseconds

--- src/base/main.cpp
+++ src/base/main.cpp
@@ -6,7 +6,10 @@
 #include <RH_NRF24.h>           //radio
 #include <RHReliableDatagram.h> //radio
 
+#include <LcdChar.h>  //LCD characters
+
 //Radio - transceiver
+//#define CLIENT_ADDRESS 1  //controller
 //#define SERVER_ADDRESS 2  //base
 //delcare the radio driver to use
 //RH_NRF24 driver(48, 53);   //CE, CSN
@@ -18,15 +21,33 @@ RH_NRF24 driver(53, 48);   //CE, CSN
 const int rs=22, en=24, d4=26, d5=28, d6=30, d7=32;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-//Initialize the servos
-#define SERVO_1   7
+//Initialize the servos - PCD silkscreen error 8, 9, 10 are actually 11, 12, 13
+#define SERVO_1   11  //8
+#define SERVO_2   12  //9
+#define SERVO_3   13  //10
+#define SERVO_4   8   //11
+#define SERVO_5   9   //12
+#define SERVO_6   10  //13
 Servo servo1;
-int servo1_angle = 90;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+uint16_t servo1_angle = 1500;   //waist rotation
+uint16_t servo2_angle = 900;    //shoulder elevation
+uint16_t servo3_angle = 1800;   //elbow elevation
+uint16_t servo4_angle = 1800;   //wrist elevation
+uint16_t servo5_angle = 1600;   //wrist rotation
+uint16_t servo6_angle = 1100;   //gripper open
 
 //Declare the return data: link status, base state
 uint8_t data[2];
 //Declare the message buffer
 uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+
+//should debug serial lines be printed?
+bool debug = true;
 
 void setup() {
   Serial.begin(9600);
@@ -37,6 +58,14 @@ void setup() {
   lcd.begin(16, 2);
   lcd.clear();
   lcd.print("Initializing...");
+  //create lcd characters
+  lcd.createChar(0, CharUp);
+  lcd.createChar(1, CharDown);
+  lcd.createChar(2, CharLeft);
+  lcd.createChar(3, CharRight);
+  lcd.createChar(4, CharSignalFull);
+  lcd.createChar(5, CharSignalPartial);
+  lcd.createChar(6, CharX);
 
   //initialize nrf24 object
   if (!driver.init())
@@ -51,12 +80,22 @@ void setup() {
     //Serial.println("nrf24 setRF failed");
 
   //attach the servos
-  servo1.attach(SERVO_1);
+  servo1.attach(SERVO_1, 550, 2400);
+  servo2.attach(SERVO_2, 600, 2100);
+  servo3.attach(SERVO_3, 1000, 2400);
+  servo4.attach(SERVO_4, 900, 2400);
+  servo5.attach(SERVO_5, 550, 2400);
+  servo6.attach(SERVO_6, 1000, 2000);
+  //move servos to home position
   servo1.write(servo1_angle);
+  servo2.write(servo2_angle);
+  servo3.write(servo3_angle);
+  servo4.write(servo4_angle);
+  servo5.write(servo5_angle);
+  servo6.write(servo6_angle);
 
   Serial.println("Initialized and receiving...");
   lcd.clear();
-  lcd.print("LK:");
 }
 
 void loop() {
@@ -68,52 +107,82 @@ void loop() {
     uint8_t from;
     if (driver.recv(buf, &len)) {
     //if (manager.recvfromAck(buf, &len, &from)) {
-      Serial.print("Response from 0x");
-      Serial.print(from, HEX);
-      Serial.print(" - LX:");
-      Serial.print(buf[0]);
-      Serial.print("\tLY:");
-      Serial.print(buf[1]);
-      Serial.print("\tLSW:");
-      Serial.print(buf[2]);
-      Serial.print("\tRX:");
-      Serial.print(buf[3]);
-      Serial.print("\tRY:");
-      Serial.print(buf[4]);
-      Serial.print("\tRSW:");
-      Serial.print(buf[5]);
+      if (debug) {
+        Serial.print("Response from 0x");
+        Serial.print(from, HEX);
+        Serial.print(" - 1: ");
+        Serial.print(servo1_angle);
+        Serial.print("\t2: ");
+        Serial.print(servo2_angle);
+        Serial.print("\t3: ");
+        Serial.print(servo3_angle);
+        Serial.print("\t4: ");
+        Serial.print(servo4_angle);
+        Serial.print("\t5: ");
+        Serial.print(servo5_angle);
+        Serial.print("\t6: ");
+        Serial.println(servo6_angle);
+        //Serial.print(" - LX:");
+        //Serial.print(buf[0]);
+        //Serial.print("\tLY:");
+        //Serial.print(buf[1]);
+        //Serial.print("\tLSW:");
+        //Serial.print(buf[2]);
+        //Serial.print("\tRX:");
+        //Serial.print(buf[3]);
+        //Serial.print("\tRY:");
+        //Serial.print(buf[4]);
+        //Serial.print("\tRSW:");
+        //Serial.println(buf[5]);
+      }
 
-      lcd.setCursor(4, 0);
-      lcd.print("            ");  //clear row
-      lcd.setCursor(4, 0);
-      lcd.print("LIVE");
-      lcd.setCursor(0, 1);
-      lcd.print("                ");  //clear row
-      lcd.setCursor(0, 1);
-      lcd.print(buf[0]);
-      lcd.print(";");
-      lcd.print(buf[1]);
-      lcd.print(";");
-      lcd.print(buf[3]);
-      lcd.print(";");
-      lcd.print(buf[4]);
-
-      if (buf[0] < 124 || buf[0] > 130) {
-        int left_joy_x_delta = map(buf[0], 0, 255, -8, 8);
-        if (servo1_angle + left_joy_x_delta > 0 &&
-            servo1_angle + left_joy_x_delta < 180) {
-          servo1_angle += left_joy_x_delta;
+      //process all servo commands
+      if (buf[0] < 120 || buf[0] > 135) {
+        int left_joy_x_delta = map(buf[0], 0, 254, -50, 50);
+        if (servo5_angle + left_joy_x_delta > 550 &&
+            servo5_angle + left_joy_x_delta < 2400) {
+          servo5_angle += left_joy_x_delta;
         }
       }
-      Serial.print("\t");
-      Serial.println(servo1_angle);
+      if (buf[1] < 120 || buf[1] > 135) {
+        int left_joy_y_delta = map(buf[1], 0, 254, -50, 50);
+        if (servo4_angle + left_joy_y_delta > 1000 &&
+            servo4_angle + left_joy_y_delta < 2400) {
+          servo4_angle += left_joy_y_delta;
+        }
+      }
+      if (buf[3] < 120 || buf[3] > 135) {
+        int right_joy_x_delta = map(buf[3], 0, 254, -50, 50);
+        if (servo1_angle + right_joy_x_delta > 550 &&
+            servo1_angle + right_joy_x_delta < 2400) {
+          servo1_angle += right_joy_x_delta;
+        }
+      }
+      if (buf[4] < 120 || buf[4] > 135) {
+        int right_joy_y_delta = map(buf[4], 0, 254, -50, 50);
+        if (servo2_angle + right_joy_y_delta > 600 &&
+            servo2_angle + right_joy_y_delta < 2100) {
+          servo2_angle += right_joy_y_delta;
+        }
+      }
 
-      data[0] = 1;
-      data[1] = 2;
+      //lcd.setCursor(15, 0);
+      //lcd.write(byte(4));
+      //lcd.setCursor(0, 1);
+      //lcd.print("                ");  //clear row
+      //lcd.setCursor(0, 1);
+      //lcd.print(servo1_angle);
+      //lcd.setCursor(4, 1);
+      //lcd.print(servo2_angle);
+      //lcd.setCursor(8, 1);
+      //lcd.print(servo3_angle);
+      //lcd.setCursor(12, 1);
+      //lcd.print(servo4_angle);
 
       //send reply back to client
       data[0] = 1;  //connection
       //data[1] = 2;  //status
+      data[1] = map(servo1_angle, 544, 2400, 0, 180);
       driver.send(data, sizeof(data));
       driver.waitPacketSent();
       //if (!manager.sendtoWait(data, sizeof(data), from)) {
@@ -121,19 +190,15 @@ void loop() {
       //}
     } else {
       Serial.println("Recv failed");
-      lcd.setCursor(6, 0);
-      lcd.print("          ");  //clear row
-      lcd.setCursor(6, 0);
-      lcd.print("FAILED");
+      lcd.setCursor(15, 0);
+      lcd.write(byte(6));
     }
     digitalWrite(13, LOW);
-  //} else {
-  //  Serial.println("no signal");
-  //  lcd.setCursor(4, 0);
-  //  lcd.print("            ");  //clear row
-  //  lcd.setCursor(4, 0);
-  //  lcd.print("NO SIGNAL");
-  //  delay(1000);
   }
-  servo1.write(servo1_angle);
+  servo1.writeMicroseconds(servo1_angle);
+  servo2.writeMicroseconds(servo2_angle);
+  servo3.writeMicroseconds(servo3_angle);
+  servo4.writeMicroseconds(servo4_angle);
+  servo5.writeMicroseconds(servo5_angle);
+  servo6.writeMicroseconds(servo6_angle);
 }
